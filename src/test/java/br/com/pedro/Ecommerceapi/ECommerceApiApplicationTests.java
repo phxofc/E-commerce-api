3d422289c1469b40b894ff3101992ee423a89f1f
phxofc <phdfrocha99@gmail.com>
2023-12-22 08:17:26 -0300

    create project

---
+++ src/test/java/br/com/pedro/Ecommerceapi/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.Ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
