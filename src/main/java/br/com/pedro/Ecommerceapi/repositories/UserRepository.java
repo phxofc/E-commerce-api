d6b5c662737c1ae4cad76617a61a3fa0aed991f5
phxofc <phdfrocha99@gmail.com>
2024-01-03 16:52:44 -0300

    add AuthorizarionService

--- src/main/java/br/com/pedro/Ecommerceapi/repositories/UserRepository.java
+++ src/main/java/br/com/pedro/Ecommerceapi/repositories/UserRepository.java
@@ -2,6 +2,10 @@ package br.com.pedro.Ecommerceapi.repositories;
 
 import br.com.pedro.Ecommerceapi.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public interface UserRepository extends JpaRepository<UserModel,Long> {}
+public interface UserRepository extends JpaRepository<UserModel,Long> {
+
+    UserDetails findByUsername(String username);
+}
