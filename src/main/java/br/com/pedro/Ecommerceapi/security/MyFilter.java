0e8c894f39618f324ee0a0b7c242454e47827045
phxofc <phdfrocha99@gmail.com>
2024-01-03 16:31:58 -0300

    add class Myfilter, MySecurityConfig and pom

---
+++ src/main/java/br/com/pedro/Ecommerceapi/security/MyFilter.java
@@ -0,0 +1,16 @@
+package br.com.pedro.Ecommerceapi.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class MyFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request,response);
+    }
+}
---
+++ src/main/java/br/com/pedro/Ecommerceapi/security/MySecurityConfig.java
@@ -0,0 +1,31 @@
+package br.com.pedro.Ecommerceapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/user").permitAll()
+                        .anyRequest().authenticated()
+                )
+                        .build();
+
+        
+
+    }
+}
