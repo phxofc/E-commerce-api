c8edeb81bbc755f0b5e3f4972008f20efa7c0567
phxofc <phdfrocha99@gmail.com>
2024-01-04 10:27:55 -0300

    adjust security

--- src/main/java/br/com/pedro/Ecommerceapi/security/SecurityConfigurations.java
+++ src/main/java/br/com/pedro/Ecommerceapi/security/SecurityConfigurations.java
@@ -31,6 +31,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
