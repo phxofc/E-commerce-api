32d04e8804f06cb7c57a854a2fada8d250cf49c7
phxofc <phdfrocha99@gmail.com>
2024-01-04 10:03:36 -0300

    validate token

--- src/main/java/br/com/pedro/Ecommerceapi/services/TokenService.java
+++ src/main/java/br/com/pedro/Ecommerceapi/services/TokenService.java
@@ -4,6 +4,7 @@ import br.com.pedro.Ecommerceapi.models.UserModel;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,20 @@ public class TokenService {
         }
 
 
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return  JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+
+        }
     }
     private Instant generateExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("03-00"));
